73a72741d02423b78bfbd29b89beb21a94137426
Pritesh Kamble <priteshkamble13@gmail.com>
2026-04-20 19:59:41 +0530

    eks.tf syntax error

--- eks.tf
+++ eks.tf
@@ -73,14 +73,14 @@ resource "aws_eks_node_group" "nodes" {
   node_group_name = "devops-nodes"
   node_role_arn   = aws_iam_role.node_role.arn
 
-  subnet_ids = [
+  
     subnet_ids = [
   aws_subnet.public_1.id,
   aws_subnet.public_2.id,
   aws_subnet.private_1.id,
   aws_subnet.private_2.id
 ]
-  ]
+  
 
   scaling_config {
     desired_size = 2
