0287747a99c88a37a7bef2af3e201f3597e0f71b
Pritesh Kamble <priteshkamble13@gmail.com>
2026-04-20 19:46:56 +0530

    main.tf edited

--- main.tf
+++ main.tf
@@ -8,24 +8,52 @@ resource "aws_vpc" "main" {
 }
 
 # Public Subnet
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1a"
 
   tags = {
-    Name = "public-subnet"
+    Name = "public-subnet-1"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/devops-eks" = "owned"
   }
 }
 
-# Private Subnet
-resource "aws_subnet" "private" {
+resource "aws_subnet" "public_2" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-south-1b"
 
   tags = {
-    Name = "private-subnet"
+    Name = "public-subnet-2"
+    "kubernetes.io/role/elb" = "1"
+    "kubernetes.io/cluster/devops-eks" = "owned"
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet-1"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/devops-eks" = "owned"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "private-subnet-2"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/devops-eks" = "owned"
   }
 }
 
@@ -41,22 +69,22 @@ resource "aws_internet_gateway" "igw" {
 # Route Table
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "public-route-table"
-  }
 }
 
-# Route
-resource "aws_route" "public_internet_access" {
+resource "aws_route" "internet_access" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
 
 # Associate Route Table with Public Subnet
-resource "aws_route_table_association" "public_assoc" {
-  subnet_id      = aws_subnet.public.id
+resource "aws_route_table_association" "public_1_assoc" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_2_assoc" {
+  subnet_id      = aws_subnet.public_2.id
   route_table_id = aws_route_table.public_rt.id
 }
 
